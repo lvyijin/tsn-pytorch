dce6a7c66d3565c130b3503e8f14ef706e656c67
lvyijin <15627860397@163.com>
2018-09-03 15:24:39 +0800

    add args.txt

--- test_models.py
+++ test_models.py
@@ -28,8 +28,8 @@ parser.add_argument('--crop_fusion_type', type=str, default='avg',
                     choices=['avg', 'max', 'topk'])
 parser.add_argument('--k', type=int, default=3)
 parser.add_argument('--dropout', type=float, default=0.7)
-parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
-                    help='number of data loading workers (default: 4)')
+parser.add_argument('-j', '--workers', default=1, type=int, metavar='N',
+                    help='number of data loading workers (default: 1)')
 parser.add_argument('--gpus', nargs='+', type=int, default=None)
 parser.add_argument('--flow_prefix', type=str, default='')
 
@@ -49,11 +49,25 @@ net = TSN(num_class, 1, args.modality,
           base_model=args.arch,
           consensus_type=args.crop_fusion_type,
           dropout=args.dropout)
-
+print net
 checkpoint = torch.load(args.weights)
-print("model epoch {} best prec@1: {}".format(checkpoint['epoch'], checkpoint['best_prec1']))
+count = 0
+base_dict = {}
+for k, v in checkpoint.items():
+    count = count + 1
+    print count, k
+    if 415>count>18:
+        base_dict.setdefault(k[7:], checkpoint[k])
+    if count<19:
+        base_dict.setdefault(k, checkpoint[k])
+base_dict.setdefault('new_fc.weight', checkpoint['base_model.fc-action.1.weight'])
+base_dict.setdefault('new_fc.bias', checkpoint['base_model.fc-action.1.bias'])
+# For kinetics dataset:
+# base_dict.setdefault('new_fc.weight', checkpoint['base_model.fc_action.1.weight'])
+# base_dict.setdefault('new_fc.bias', checkpoint['base_model.fc_action.1.bias'])
 
-base_dict = {'.'.join(k.split('.')[1:]): v for k,v in list(checkpoint['state_dict'].items())}
+#print("model epoch {} best prec@1: {}".format(checkpoint['epoch'], checkpoint['best_prec1']))
+
 net.load_state_dict(base_dict)
 
 if args.test_crops == 1:
@@ -141,7 +155,10 @@ cf = confusion_matrix(video_labels, video_pred).astype(float)
 
 cls_cnt = cf.sum(axis=1)
 cls_hit = np.diag(cf)
-
+print 'cls_hit:'
+print cls_hit
+print 'cls_cnt:'
+print cls_cnt
 cls_acc = cls_hit / cls_cnt
 
 print(cls_acc)
@@ -164,5 +181,3 @@ if args.save_scores is not None:
         reorder_label[idx] = video_labels[i]
 
     np.savez(args.save_scores, scores=reorder_output, labels=reorder_label)
-
-
